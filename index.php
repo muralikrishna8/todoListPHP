4d769cf644555daa8e861a171d0c796295fecce1
Murali Krishna <vmurali1444@gmail.com>
2016-12-25 22:05:41 +0530

    styling the todo app

--- index.php
+++ index.php
@@ -1,43 +1,31 @@
 <html xmlns="http://www.w3.org/1999/html">
 <head>
     <title>Todo List</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-	<div> Welcome to TODO </div>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>id</th>
-                <th>task</th>
-                <th>date</th>
-            </tr>
-        </thead>
-        <tbody>
+    <br /   >
+    <ul class="todo-container">
         <?php require("./connect.php"); ?>
-    <?php
-        $query = mysqli_query($mysqlConnection, "SELECT * FROM todos");
-        while($row = mysqli_fetch_assoc($query)) {
-            $task_number = $row['id'];
-            $task = $row['task'];
-            $date = $row['date'];
-            print <<<END
-                <tr>
-                    <td>$task_number</td>
-                    <td>$task</td>
-                    <td>$date</td>
-                    <td class="delete" task="$task_number">X</td>
-                </tr>
+        <?php
+            $query = mysqli_query($mysqlConnection, "SELECT * FROM todos");
+            while($row = mysqli_fetch_assoc($query)) {
+                $task_number = $row['id'];
+                $task = $row['task'];
+                $date = $row['date'];
+                print <<<END
+                    <li class="todo">
+                        <span class="title">$task</span>
+                        <span class="delete" task="$task_number">X</span>
+                    </li>
 END;
 
-        }
-    ?>
-        </tbody>
-    </table>
-    <br />
-    <br />
-    <form action="addToDo.php" method="post">
-        <input type="text" name="task" size="50"/>
-        <input type="submit" />
+            }
+        ?>
+    </ul>
+    <form action="addToDo.php" method="post" class="new-todo-form">
+        <input type="text" class="todo-input" name="task" size="50"/>
+        <input type="submit" class="add-todo" value="add"/>
     </form>
     <script src="jquery.js"></script>
     <script>
